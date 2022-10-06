f1f19b8d8f89e96fb18511f76aec9eceb6ce2753
Johnathon Wright <jw@mustmodify.com>
2022-10-06 16:21:49 -0400

    Rails now has its own Ruby handler.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,16 @@ class Post < ApplicationRecord
   validates_length_of :body, :minimum => 10
 
   has_many :comments
+
+  def published
+    self.published_at
+  end
+
+  def published=(value)
+    if value
+      self.publised_at ||= Time.now
+    else
+      self.published_at = nil
+    end
+  end
 end
